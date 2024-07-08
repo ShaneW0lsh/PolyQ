dabb3d32d03d6856e2716f58d4b9f6b0a673fa52
Shane <shanebetterfather@gmail.com>
2024-07-08 14:40:08 +0300

    Add basic security with daoauthprovider

--- src/main/java/ru/kolomych/polyq/config/WebSecurityConfig.java
+++ src/main/java/ru/kolomych/polyq/config/WebSecurityConfig.java
@@ -1,24 +1,57 @@
 package ru.kolomych.polyq.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import ru.kolomych.polyq.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    private final SuccessUserHandler successUserHandler;
+    private final UserService userService;
+
+    @Autowired
+    public WebSecurityConfig(SuccessUserHandler successUserHandler, UserService userService) {
+        this.successUserHandler = successUserHandler;
+        this.userService = userService;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
-                        .anyRequest().permitAll()
+            .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
                 )
-                .csrf().disable()
-                .httpBasic(Customizer.withDefaults());
+                .formLogin(form -> form
+                        .successHandler(successUserHandler)
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(userService);
+        return authenticationProvider;
     }
 }
--- src/main/java/ru/kolomych/polyq/service/UserService.java
+++ src/main/java/ru/kolomych/polyq/service/UserService.java
@@ -1,6 +1,11 @@
 package ru.kolomych.polyq.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kolomych.polyq.model.Role;
@@ -9,11 +14,13 @@ import ru.kolomych.polyq.repository.UserRepository;
 import ru.kolomych.polyq.util.NotFoundException;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final RoleService roleService;
@@ -53,5 +60,19 @@ public class UserService {
     public void deleteUser(String username) throws NotFoundException {
         User user = findUserByUsername(username);
         userRepository.delete(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isEmpty())
+            throw new UsernameNotFoundException(String.format("Username `%s` not found", username));
+        return new org.springframework.security.core.userdetails.User(
+                user.get().getUsername(), user.get().getPassword(), mapRolesToAuthorities(user.get().getRoles())
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 }
