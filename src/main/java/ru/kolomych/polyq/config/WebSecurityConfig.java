08b294c4535adf35d9d508c5f6f358e6d96a7fb3
Shane <shanebetterfather@gmail.com>
2024-07-08 17:08:12 +0300

    Add basic security with daoauthprovider

--- src/main/java/ru/kolomych/polyq/config/WebSecurityConfig.java
+++ src/main/java/ru/kolomych/polyq/config/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
